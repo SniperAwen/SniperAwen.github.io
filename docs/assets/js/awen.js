3a2d82576004d91141209b0282db7cd46234195f
Awen <2889984509@qq.com>
2022-02-22 23:47:11 +0800

    edit

--- docs/assets/js/awen.js
+++ docs/assets/js/awen.js
@@ -10,6 +10,11 @@
 /_/          \\_\\    \\__/      \\__/        \\________|    |__|     |__|
 `
 ); */
+// window.onload = () => {
+//   document.querySelectorAll("footer span")[1].addEventListener("click", () => {
+//     alert("(❤ ω ❤)");
+//   });
+// };
 console.log(
   "%c Awen",
   "color:white;font-size:150px;text-shadow: 0px 0px 5px green;"
